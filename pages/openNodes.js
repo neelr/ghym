f867960c685154f7fa9bcb9a82415716d80ad2d1
Neel Redkar <neel.redkar@outlook.com>
2020-02-29 14:55:34 -0800

    Revert "hi"
    This reverts commit 4aa54b462fcf88d3fd01a1674e5684a924dcabbb.

--- pages/openNodes.js
+++ pages/openNodes.js
@@ -3,9 +3,11 @@ import {Box, Flex, Text, Heading, Button} from "rebass";
 
 
 export default class open_nodes extends React.Component{
-    render(){
-        return(
-            <Flex flexDirection="column"></Flex>
+    render () {
+        return (
+            <Flex flexDirection="column">
+
+            </Flex>
         )
     }
 }
