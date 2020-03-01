b98a686ba4ab465ac6fd1c42738ddf42abe45a41
Neel Redkar <neel.redkar@outlook.com>
2020-02-29 23:29:05 -0800

    take out devtools

--- app.js
+++ app.js
@@ -38,7 +38,6 @@ function createWindow () {
 				protocol: 'file',
 				slashes: true
 		}))
-		win.webContents.openDevTools();
 	 } else {
 			 win.loadURL("http://localhost:3000");
 			 win.webContents.openDevTools();
